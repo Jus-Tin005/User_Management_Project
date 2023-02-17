913743f6958fca372fc8501ef31343966b05e15a
Khun Tun(Jus Tin) <jajahuge890@gmail.com>
2023-02-17 11:26:09 +0630

    second updated

--- User_Management_System_Project/User_Management_System_Project/resources/views/admin/body/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="cols-center">
-        <div class="rows">
-                        <div class="cols-12">
-                                Copyright @<script>document.write(new Date().getFullYear());</script>
-                                All Rights Reserved by<b> Jio</b>
-                        </div>
-
-        </div>
-</footer>
--- User_Management_System_Project/resources/views/users/create.blade.php
+++ User_Management_System_Project/resources/views/users/create.blade.php
@@ -1,68 +1,85 @@
 @extends('layouts.app')
 @section('admin')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Create New User</h2>
-        </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('users.index') }}"> Back</a>
-        </div>
-    </div>
-</div>
+<section class="container my-3">
+             <div class="table-wrapper">
+                    <div class="table-title">
+                                <div class="row">
+                                    <div class="col-12">
+                                            <h5>Add New User</h5>
+                                    </div>
+                                    <div class="col-12  d-flex justify-content-end">
+                                                <a class="btn btn-primary" href="{{ route('users.index') }}">Back</a>
+                                    </div>
+                        </div>
+                    </div>
+            </div>
+
+            @if (count($errors) > 0)
+            <div class="alert alert-danger">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+                </ul>
+            </div>
+            @endif
+
+            {!! Form::open(array('route' => 'users.store','method'=>'POST')) !!}
+            <div class="main-section">
+                <div class="row">
+                        <div class="col-12">
+                                <div class="card">
+                                        <div class="card-body">
+                                                <div class="row mb-3">
+                                                        <label for="name" class="col-sm-3 col-form-label"><strong>Name :</strong></label>
+                                                        <div class="col-sm-9">
+                                                                {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
+                                                        </div>
+                                                </div>
 
 
-@if (count($errors) > 0)
-  <div class="alert alert-danger">
-    <strong>Whoops!</strong> There were some problems with your input.<br><br>
-    <ul>
-       @foreach ($errors->all() as $error)
-         <li>{{ $error }}</li>
-       @endforeach
-    </ul>
-  </div>
-@endif
+                                                <div class="row mb-3">
+                                                      <label for="name" class="col-sm-3 col-form-label"><strong>Email :</strong></label>
+                                                        <div class="col-sm-9">
+                                                                    {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
+                                                        </div>
+                                                </div>
+
+                                                    <div class="row mb-3">
+                                                        <label for="name" class="col-sm-3 col-form-label"><strong>Password :</strong></label>
+                                                        <div class="col-sm-9">
+                                                                {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row mb-3">
+                                                        <label for="name" class="col-sm-3 col-form-label"><strong>Confirm Password :</strong></label>
+                                                        <div class="col-sm-9">
+                                                                {!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' => 'form-control')) !!}
+                                                        </div>
+                                                    </div>
+
+
+                                                    <div class="row mb-3">
+                                                        <label for="name" class="col-sm-3 col-form-label"><strong>Role :</strong></label>
+                                                        <div class="col-sm-9">
+                                                                {!! Form::select('roles[]', $roles,[], array('class' => 'form-control','multiple')) !!}
+                                                        </div>
+                                                    </div>
+                                                    <div class="btn-group d-flex justify-content-end">
+                                                                <button type="submit" class="btn btn-primary">Submit</button>
+                                                    </div>
+                                        </div>
+                                </div>
+                        </div>
+                </div>
+        </div>
+             {!! Form::close() !!}
+            @endsection
+</section>
 
 
 
-{!! Form::open(array('route' => 'users.store','method'=>'POST')) !!}
-<div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Name:</strong>
-            {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Email:</strong>
-            {!! Form::text('email', null, array('placeholder' => 'Email','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Password:</strong>
-            {!! Form::password('password', array('placeholder' => 'Password','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Confirm Password:</strong>
-            {!! Form::password('confirm-password', array('placeholder' => 'Confirm Password','class' => 'form-control')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12">
-        <div class="form-group">
-            <strong>Role:</strong>
-            {!! Form::select('roles[]', $roles,[], array('class' => 'form-control','multiple')) !!}
-        </div>
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </div>
-</div>
-{!! Form::close() !!}
 
 
-<p class="text-center text-primary"><small>Tutorial by ItSolutionStuff.com</small></p>
-@endsection
